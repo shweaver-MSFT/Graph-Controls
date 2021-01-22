8cef761167ba85050db547a984d1c9681c6a57d9
Shane Weaver <shweaver@microsoft.com>
2021-01-22 15:48:31 -0800

    Added rename feature for tasks

--- Microsoft.Toolkit.Graph.Controls/Controls/TaskItem/TaskItem.Properties.cs
+++ Microsoft.Toolkit.Graph.Controls/Controls/TaskItem/TaskItem.Properties.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Graph;
+using System;
+using System.ComponentModel;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 
 namespace Microsoft.Toolkit.Graph.Controls
@@ -21,7 +24,18 @@ namespace Microsoft.Toolkit.Graph.Controls
         /// Todo task item metadata.
         /// </summary>
         public static readonly DependencyProperty TaskDetailsProperty =
-            DependencyProperty.Register(nameof(TaskDetails), typeof(TodoTask), typeof(TaskItem), new PropertyMetadata(null));
+            DependencyProperty.Register(nameof(TaskDetails), typeof(TodoTask), typeof(TaskItem), new PropertyMetadata(null, OnTaskDetailsChanged));
+
+        private static async void OnTaskDetailsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await d.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (d is TaskItem taskItem)
+                {
+                    taskItem.TaskTitle = taskItem.TaskDetails.Title;
+                }
+            });
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether the control is in an editable state.
@@ -36,6 +50,53 @@ namespace Microsoft.Toolkit.Graph.Controls
         /// Edit mode enablement property.
         /// </summary>
         public static readonly DependencyProperty IsEditModeEnabledProperty =
-            DependencyProperty.Register(nameof(IsEditModeEnabled), typeof(bool), typeof(TaskItem), new PropertyMetadata(false));
+            DependencyProperty.Register(nameof(IsEditModeEnabled), typeof(bool), typeof(TaskItem), new PropertyMetadata(false, OnEditModeChanged));
+
+        private static async void OnEditModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await d.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (d is TaskItem taskItem)
+                {
+                    if (taskItem.IsEditModeEnabled)
+                    {
+                        taskItem._taskTitleInputTextBox.Text = taskItem.TaskTitle;
+                        taskItem._taskTitleInputTextBox.Focus(FocusState.Programmatic);
+                    }
+                    else
+                    {
+                        // Save the updates
+                        taskItem.TaskTitle = taskItem._taskTitleInputTextBox.Text;
+                        taskItem._taskTitleInputTextBox.Text = string.Empty;
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TaskTitle
+        {
+            get { return (string)GetValue(TaskTitleProperty); }
+            set { SetValue(TaskTitleProperty, value); }
+        }
+
+        /// <summary>
+        /// Task title input text property.
+        /// </summary>
+        public static readonly DependencyProperty TaskTitleProperty =
+            DependencyProperty.Register(nameof(TaskTitle), typeof(string), typeof(TaskItem), new PropertyMetadata(string.Empty, OnTaskTitleChanged));
+
+        private static async void OnTaskTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            await d.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (d is TaskItem taskItem)
+                {
+                    taskItem.TaskDetails.Title = taskItem.TaskTitle;
+                }
+            });
+        }
     }
 }
--- Microsoft.Toolkit.Graph.Controls/Controls/TaskItem/TaskItem.cs
+++ Microsoft.Toolkit.Graph.Controls/Controls/TaskItem/TaskItem.cs
@@ -1,18 +1,50 @@
 ﻿using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.Toolkit.Graph.Controls
 {
     /// <summary>
     /// A visualization of a single Todo task item.
     /// </summary>
+    [TemplatePart(Name = TaskTitleInputTextBoxPart, Type = typeof(TextBox))]
     public partial class TaskItem : BaseGraphControl
     {
+        private const string TaskTitleInputTextBoxPart = "PART_TaskTitleInputTextBox";
+
+        private TextBox _taskTitleInputTextBox;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="TaskItem"/> class.
         /// </summary>
         public TaskItem()
         {
             this.DefaultStyleKey = typeof(TaskItem);
+        }
+
+        /// <summary>
+        ///  <inheritdoc />
+        /// </summary>
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (_taskTitleInputTextBox != null)
+            {
+                _taskTitleInputTextBox.LostFocus -= TaskTitleInputTextBox_LostFocus;
+            }
+
+            _taskTitleInputTextBox = GetTemplateChild(TaskTitleInputTextBoxPart) as TextBox;
+
+            if (_taskTitleInputTextBox != null)
+            {
+                _taskTitleInputTextBox.LostFocus += TaskTitleInputTextBox_LostFocus;
+            }
+        }
+
+        private void TaskTitleInputTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            IsEditModeEnabled = false;
         }
 
         /// <inheritdoc/>
